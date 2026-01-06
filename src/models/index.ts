e27388acb292c9b11602279ca953beae7bd126f3
Camilla Barros <camillaerbarros@gmail.com>
2026-01-06 17:16:27 -0300

    feat: cria controller de trainers

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,9 +7,23 @@ const Users = UsersModel(sequelize);
 const Leads = LeadsModel(sequelize);
 const Trainers = TrainersModel(sequelize);
 
+// ---- Relações ----
+
+// 1:1 -> Um Usuário pode ter um Perfil de Treinador
+Users.hasOne(Trainers, {
+  foreignKey: "user_id",
+  as: "trainer_profile",
+});
+
+// 1:1 -> Um Treinador pertence a um Usuário
+Trainers.belongsTo(Users, {
+  foreignKey: "user_id",
+  as: "user",
+});
+
 async function syncDatabase() {
   try {
-    await sequelize.sync();
+    await sequelize.sync({ alter: true }); // incluir o alter??
   } catch (error) {
     console.log("Error: " + (error as Error).message);
   }
