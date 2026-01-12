d2a9fe2333b723527f3f9d5f89e9693d254e162b
THIAGO <tramos222@hotmail.com>
2026-01-12 15:35:11 -0300

    feat: add user_roles model and controller

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,11 @@
 import sequelize from "../config/config.ts";
 import UsersModel from "./Users.ts";
 import LeadsModel from "./Leads.ts";
+import UsersRolesModel from "./Users_Roles.ts";
 
 const Users = UsersModel(sequelize);
 const Leads = LeadsModel(sequelize);
+const UsersRoles = UsersRolesModel(sequelize);
 
 async function syncDatabase() {
   try {
@@ -15,4 +17,4 @@ async function syncDatabase() {
 
 syncDatabase();
 
-export { Leads, Users };
+export { Leads, Users, UsersRoles };
